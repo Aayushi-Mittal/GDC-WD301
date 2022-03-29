1f1d98b35e25f4856dbbc5fef143fa62bcdff328
Aayushi Mittal <aayushimittal088@gmail.com>
2022-03-30 01:58:57 +0530

    Added Home component and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import AppContainer from './components/AppContainer';
 import Header from './components/Header';
 import Form from './components/Form';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState("HOME");
+
   return (
     <AppContainer>
-      <Header title={"Welcome to 5 #react-typescript with #tailwindcss"}/>
-      <Form/>
+      <Header title={"Welcome to 5 #react-typescript with #tailwindcss"} />
+      {
+      state === "HOME" ? 
+        (<><Home/> <button className="mt-4 p-2 bg-gray-100 border-2 border-cyan-200 rounded-lg hover:bg-cyan-200 w-full" onClick={()=>setState("FORM")}>Open Form</button></>) : 
+        (<><Form/> <button className="mt-4 p-2 bg-gray-100 border-2 border-cyan-200 rounded-lg hover:bg-cyan-200 w-full" onClick={()=>setState("HOME")}>Close Form</button></>)
+      }
     </AppContainer>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p className="flex justify-center text-xl my-4 font-medium">Welcome to the Home Page of the Quiz</p>
+    </div>
+  )
+}
+
+export default Home
