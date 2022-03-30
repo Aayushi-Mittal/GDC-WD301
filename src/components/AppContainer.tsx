7c7bace9b97b916d27fbf7addf42d62d5120c184
Aayushi Mittal <aayushimittal088@gmail.com>
2022-03-30 20:50:23 +0530

    Updated css of AppContainer

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const AppContainer = (props: {children: React.ReactNode}) => {
   return (
-    <div className="flex h-screen bg-cyan-200 items-center">
-      <div className=" p-8 mx-auto bg-white shadow-lg rounded-xl">
+    <div className="flex p-6 bg-cyan-200 min-h-screen items-center">
+      <div className="p-8 mx-auto bg-white shadow-lg rounded-xl">
         {props.children}
       </div>
     </div>
