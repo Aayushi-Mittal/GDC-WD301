a7bb03708fef43ffa38d1aba8401d617657c11de
Aayushi Mittal <aayushimittal088@gmail.com>
2022-03-30 20:42:36 +0530

    Add Field Functionality completed

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import LabelledInput from "./LabelledInput";
-import {useState} from 'react'
+import { useState } from 'react'
 
 const formFields = [
   { id: 1, label: "First Name", type: "text" },
@@ -9,35 +9,55 @@ const formFields = [
   { id: 5, label: "Phone Number", type: "tel" },
 ];
 
-function Form(props:{closeFormCB:()=>void}) {
+function Form(props: { closeFormCB: () => void }) {
   const [state, setState] = useState(formFields);
+  const [newField, setNewField] = useState("");
+  const [newFieldType, setNewFieldType] = useState("");
 
   const addField = () => {
     setState([
       ...state,
       {
-        id: state.length+2,
-        label: "New Field",
-        type: "text"
+        id: Number(new Date()),
+        label: newField,
+        type: newFieldType
       }
     ])
+    setNewFieldType("text");
   }
 
-  const removeField = (id:number) => {
+  const removeField = (id: number) => {
     setState(state.filter((field) => field.id !== id))
   }
 
   return (
-    <>
-      <form className="mt-6 mb-4">
-        {state.map((field) => (
-          <LabelledInput key={field.id} id={field.id} label={field.label} type={field.type} removeFieldCB={removeField}/>
-        ))}
-        <input type="submit" className="p-2 mt-6 bg-cyan-200 rounded-lg w-full hover:bg-cyan-300" />
-        <button className="mt-4 p-2 bg-gray-100 border-2 border-cyan-200 rounded-lg hover:bg-cyan-200 w-1/2" onClick={props.closeFormCB}>Close Form</button>
-        <button className="mt-4 p-2 bg-gray-100 border-2 border-cyan-200 rounded-lg hover:bg-cyan-200 w-1/2" onClick={addField}>Add Field</button>
-      </form>
-    </>
+    <div className="mt-6">
+      {state.map((field) => (
+        <LabelledInput key={field.id} id={field.id} label={field.label} type={field.type} removeFieldCB={removeField} />
+      ))}
+      <div className="border-2 rounded-lg p-4 mt-5 border-cyan-200">
+        <label className="font-semibold">Add Field</label>
+        <div className="flex justify-between gap-2">
+          <input type="text"
+            className="p-2 mt-2 bg-gray-100 rounded-lg outline-cyan-500 w-3/5"
+            placeholder="Add New Field"
+            onChange={(e) => {
+              e.preventDefault();
+              setNewField(e.target.value);
+            }} />
+          <input type="text"
+            className="p-2 mt-2 bg-gray-100 rounded-lg outline-cyan-500 w-2/5"
+            placeholder="Enter Field Type"
+            onChange={(e) => {
+              e.preventDefault();
+              setNewFieldType(e.target.value);
+            }} />
+          <button className="p-2 mt-2 bg-cyan-200 rounded-lg hover:bg-cyan-300" onClick={addField}>Add</button>
+        </div>
+      </div>
+      <input type="submit" className="p-2 mt-6 bg-cyan-200 rounded-lg w-full hover:bg-cyan-300" />
+      <button className="p-2 mt-6 bg-cyan-200 rounded-lg hover:bg-cyan-300 w-full" onClick={props.closeFormCB}>Close Form</button>
+    </div>
   );
 }
 
--- src/components/LabelledInput.tsx
+++ src/components/LabelledInput.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
+const LabelledInput = (props: { id: number, label: string, type: string, removeFieldCB: (id: number) => void }) => {
 
-const LabelledInput = (props:{id:number, label:string, type:string, removeFieldCB: (id:number)=>void}) => {
-  return (
-    <div key={props.id} className="py-2">
-        <div className="flex justify-between">
-            <label className="font-semibold">{props.label}</label>
-            <button className="text-cyan-500" onClick={(_)=>props.removeFieldCB(props.id)}>Remove Field</button>
+    return (
+        <div key={props.id} className="py-2">
+            <div className="flex justify-between">
+                <label className="font-semibold">{props.label}</label>
+                <button className="text-cyan-500" onClick={(_) => { props.removeFieldCB(props.id) }}>Remove Field</button>
+            </div>
+            <input
+                type={props.type}
+                className="p-2 bg-gray-100 rounded-lg outline-cyan-500 w-full"
+            />
         </div>
-        <input
-          type={props.type}
-          className="p-2 bg-gray-100 rounded-lg outline-cyan-500 w-full"
-        />
-    </div>
-  )
+    )
 }
 
 export default LabelledInput;
